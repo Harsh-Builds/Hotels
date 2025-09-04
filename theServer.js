2d52d7e11aad1398de47676342c4ad6cb270defd
Harsh-Builds <harshsaini888555@gmail.com>
2025-09-04 13:56:45 +0530

    Adds a log middleware function

--- theServer.js
+++ theServer.js
@@ -9,6 +9,15 @@ app.use(bodyParser.json()) // req.body :- store here 'req.body' after converting
 
 const PORT = process.env.PORT || 3000 ;  //It lets your server run on the hosting platform’s assigned port if available, otherwise falls back to port 3000 for local development.
 
+
+// Middleware Function:-    A middleware function in Express is used to track, log, or control requests (like monitoring user traffic) before passing them to the next handler.
+const logRequest = (req, res , next) =>{
+  console.log(`[${new Date().toLocaleString()}] Request from : ${req.originalUrl} `);
+  next();  // move on the next phase.
+}
+
+app.use(logRequest); // so, here we use our middleware.
+
 app.get('/', (req, res) => {
   res.send('Welcome to our Hotel');
 })
